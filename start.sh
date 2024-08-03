056df7ddfcf6ce9dc4e1617fb9c394b8ec9236bd
mushinbush <markakisfiction61533@gmail.com>
2024-08-03 18:50:22 +0800

    feat: Add a new shell script for Linux

---
+++ start.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+cd "$(dirname "$0")" || exit
+
+if [ ! -d "venv" ]; then
+    echo "No venv found! Create venv and install dependencies now."
+    python3 -m venv venv
+    source venv/bin/activate
+    echo "Installing dependencies from requirements.txt"
+    pip install -r requirements.txt
+    python3 discordbot_gemini.py
+else
+    source venv/bin/activate
+    python3 discordbot_gemini.py
+fi
